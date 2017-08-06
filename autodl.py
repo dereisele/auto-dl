ed8ac5cae2b609399292f5a77d7875821f7a9fb7
Alexander Eisele <alex.eisele99@gmail.com>
2017-08-06 11:48:22 +0200

    Added downloader

--- autodl.py
+++ autodl.py
@@ -1,11 +1,12 @@
 """Main file."""
 from pluginbase import PluginBase
 from dbmanager import DBManager
+from downloader import Downloader
 import configparser
 import os
 import sys
 
-config = configparser.ConfigParser()
+
 
 
 
@@ -14,16 +15,17 @@ class AutoDl(object):
 
     scrappers = {}
     myDB = ""
+    config = configparser.ConfigParser()
 
     plugin_base = PluginBase(package='autodl.scrapper')
 
     def __init__(self):
         """Init main class."""
         doScrap = True
-        
+
         self.load_config()
         self.init_config()
-        self.myDB = DBManager(config["BASIC"]["DBLocation"])
+        self.myDB = DBManager(self.config["BASIC"]["DBLocation"])
         self.plugin_source = self.plugin_base.make_plugin_source(
                              searchpath=['./scrappers'])
 
@@ -31,61 +33,74 @@ class AutoDl(object):
             plugin = self.plugin_source.load_plugin(plugin_name)
             plugin.Scrapper().setup(self)
 
-        if len(sys.argv) == 2:
-            doScrap = sys.argv[1] == "-noscrap"
+        doScrap = "-noscrap" not in sys.argv
+        doDl = "-nodl" not in sys.argv
+
+        if "-add" in sys.argv:
+            i = sys.argv.index("-add")
+            newShow = sys.argv[i + 1]
+            newLang = sys.argv[i + 2]
+            self.myDB.addShowToDL(newShow, newLang)
 
         if doScrap:
             self.scrap()
+
         self.save_config()
+        self.myDB.matchEpisodes()
+
+        if doDl:
+            self.myDL = Downloader(self)
+            self.myDL.downloadQueue()
 
     def init_config(self):
         """Setup config if nor exist."""
         basic_conf = {"DefaultLang": "de",
                       "DefaultLocation": "de",
-                      "DBLocation": "./media.db"}
+                      "DBLocation": "./media.db",
+                      "MediaLocation": "./media"}
 
         network_conf = {"VPNStart": "windscribe connect {loc}",
                         "VPNStop": "windscribe disconnect"}
 
-        if "BASIC" not in config.sections():
-            config.add_section("BASIC")
+        if "BASIC" not in self.config.sections():
+            self.config.add_section("BASIC")
 
-        if "NETWORK" not in config.sections():
-            config.add_section("NETWORK")
+        if "NETWORK" not in self.config.sections():
+            self.config.add_section("NETWORK")
 
         for element in basic_conf:
-            if element not in config["BASIC"]:
-                config.set("BASIC", element, basic_conf[element])
+            if element not in self.config["BASIC"]:
+                self.config.set("BASIC", element, basic_conf[element])
 
         for element in network_conf:
-            if element not in config["NETWORK"]:
-                config.set("NETWORK", element, network_conf[element])
+            if element not in self.config["NETWORK"]:
+                self.config.set("NETWORK", element, network_conf[element])
 
     def load_config(self):
         """Load config from config.ini."""
         if os.path.isfile('config.ini'):
             with open('config.ini', 'r') as configfile:
-                config.read_file(configfile)
+                self.config.read_file(configfile)
 
     def save_config(self):
         """Save config in config.ini."""
         with open('config.ini', 'w') as configfile:
-            config.write(configfile)
+            self.config.write(configfile)
 
     def register_scrapper(self, name, scrapper):
         """Use to register a scrapper plugin."""
         self.scrappers[name] = scrapper
 
-        if "SCRAPPER" not in config.sections():
-            config.add_section("SCRAPPER")
+        if "SCRAPPER" not in self.config.sections():
+            self.config.add_section("SCRAPPER")
 
-        if name not in config["SCRAPPER"]:
-            config["SCRAPPER"][name] = "True"
+        if name not in self.config["SCRAPPER"]:
+            self.config["SCRAPPER"][name] = "True"
 
     def scrap(self):
         """Execute all scrappers."""
         for name, s in sorted(self.scrappers.items()):
-            if config["SCRAPPER"][name] == "True":
+            if self.config["SCRAPPER"][name] == "True":
                 print('{}: {}'.format(name, s()))
                 print('')
 
---
+++ downloader.py
@@ -0,0 +1,46 @@
+import youtube_dl
+import os
+import subprocess
+
+
+
+class Downloader(object):
+    dl_config = dict()
+
+    def __init__(self, app):
+        self.parent = app
+        self.dl_config = dict(self.parent.config)
+
+    def downloadQueue(self):
+        queue = self.parent.myDB.getEpisodesToDL()
+        for item in queue:
+            self.download(item)
+
+
+    def download(self, item):
+        dl_id, episode_id, season, episode, name, url, loc, _, state, show = item
+
+        if not loc == self.dl_config["BASIC"]["defaultlocation"]:
+            cmdline = self.dl_config["NETWORK"]["vpnstart"].format(loc=loc)
+            proc = subprocess.Popen(cmdline, stdout=subprocess.PIPE, stderr=FNULL)
+            time.sleep(2)
+
+        season = "%02d" % season
+        episode = "%02d" % episode
+
+        os.makedirs(self.dl_config["BASIC"]["medialocation"] + "/" + show.replace(" ", "-"), exist_ok=True)
+        os.chdir(self.dl_config["BASIC"]["medialocation"] + "/" + show.replace(" ", "-"))
+
+        self.parent.myDB.updateDlState(episode_id, "1")
+
+        ydl_opts = dict()
+        ydl_opts["outtmpl"] = "{}_{}_S{}E{}.%(ext)s".format(show.replace(" ", "-"), name.replace(" ", "-"), season, episode)
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        self.parent.myDB.removeEpisodeFromDL(episode_id)
+        self.parent.myDB.updateEpisodeDlState(episode_id, "1")
+
+        if not loc == self.dl_config["BASIC"]["defaultlocation"]:
+            cmdline = self.dl_config["NETWORK"]["vpnstop"]
+            proc = subprocess.Popen(cmdline,
+                                    stdout=subprocess.PIPE)
